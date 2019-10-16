5d11e641977e606910dea8b7f88f5a12fec8e184
Chemllei Joseph <chemllei0304@gmail.com>
2019-10-15 21:59:13 -0400

    write results to file

--- musicAnalyzer.py
+++ musicAnalyzer.py
@@ -2,6 +2,10 @@ import pandas as pd
 
 dataframe = pd.read_csv("data.csv")
 
-avgPlays = dataframe.groupby("albumTitle").mean().sort_values(by = "plays", ascending=False)[:10]
+avgPlays = dataframe.groupby("albumTitle").mean().sort_values(by = "plays", ascending=False)[:50]
 
-print(avgPlays)
+statsList = open("stats.txt", "w")
+statsList.write(avgPlays.to_string())
+statsList.close
+
+print("done")
