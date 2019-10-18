9d67433135fa1387fde4300c393f40fd97248b3d
Chemllei Joseph <chemllei0304@gmail.com>
2019-10-18 02:18:51 -0400

    started gui,  added new functions

--- musicAnalyzer.py
+++ musicAnalyzer.py
@@ -1,23 +1,67 @@
 import pandas as pd
 from song import song
 import random
+import Tkinter
 
 dataframe = pd.read_csv("data.csv")
-songList = []
 
-#for index, row in dataframe.iterrows():
-#  x = song(row.songName, row.artist, row.album, row.genre, row.yearReleased, row.plays)
-#  songList.append(x)
-#
-#y = random.choice(songList)
-#print(y.getAlbum())
+songList = [song(row["songName"],
+                 row["artist"],
+                 row["albumTitle"],
+                 row["genre"],
+                 row["yearReleased"],
+                 row["plays"])
+            for index, row in dataframe.iterrows()]
 
-avgPlays = dataframe.groupby("albumTitle").mean().sort_values(by = "plays", ascending=True)[:10]
-numArtists = dataframe.groupby("artist")["songName"].nunique().sort_values(ascending=False)[:10]
+def getRandomAlbums(num):
+  resultBox.delete(0, Tkinter.END)
+  for i in range(1, num + 1):
+    y = random.choice(songList)
+    resultBox.insert(Tkinter.END, y.album)
+  
+def getTopAlbums(num):
+  resultBox.delete(0, Tkinter.END)
+  avgPlays = dataframe.groupby("albumTitle").mean().sort_values(by = "plays", ascending=False)[:num]
+  print(avgPlays)
+  
+def getTopArtistsByCount(num):
+  resultBox.delete(0, Tkinter.END)
+  numArtists = dataframe.groupby("artist")["songName"].nunique().sort_values(ascending=False)[:num]
+  print(numArtists)
+  
+def exportTXT():
+  statsList = open("stats.txt", "w")
+  statsList.write(avgPlays.to_string())
+  statsList.close
+  
+def getAlbumByYear(year):
+  resultBox.delete(0, Tkinter.END)
+  results = []
+  for i in songList:
+    if i.album not in results and i.yearReleased == year:
+      results.append(i.album)
+  for i in results:
+    print(i)
+      
+mainWindow = Tkinter.Tk()
+mainWindow.title("Music DB")
 
-#statsList = open("stats.txt", "w")
-#statsList.write(avgPlays.to_string())
-#statsList.close
+numField = Tkinter.Entry(mainWindow)
+numField.pack()
 
-print(avgPlays)
-print(numArtists)
+submitButton = Tkinter.Button(mainWindow, text = "Get Random Albums", command = lambda: getRandomAlbums(int(numField.get())))
+submitButton.pack()
+
+submitButton2 = Tkinter.Button(mainWindow, text = "Get Top Albums", command = lambda: getTopAlbums(int(numField.get())))
+submitButton2.pack()
+
+submitButton3 = Tkinter.Button(mainWindow, text = "Get Top Artists", command = lambda: getTopArtistsByCount(int(numField.get())))
+submitButton3.pack()
+
+submitButton4 = Tkinter.Button(mainWindow, text = "Get Albums By Year", command = lambda: getAlbumByYear(int(numField.get())))
+submitButton4.pack()
+
+resultBox = Tkinter.Listbox(mainWindow)
+resultBox.pack()
+
+mainWindow.mainloop()
