9d6a4a06cdd0d8cf8fa20f8598831efee8e710a6
Chemllei Joseph <chemllei0304@gmail.com>
2020-02-03 22:32:20 -0500

    addalbums, viewalbums, delete(all) fn's added, cmd line gui

--- ctrla.py
+++ ctrla.py
@@ -4,3 +4,63 @@ import MySQLdb
 class ctrla():
   def __init__(self):
     pass
+  
+  def addAlbum(self, newAlbum):
+    db = MySQLdb.connect("localhost","root","bre9ase4","TESTDB")
+    cursor = db.cursor()
+    sql = "INSERT INTO albums (title, artist, genre, releaseDate, rating, tags) VALUES ('%s', '%s', '%s', '%s', '%d', '%s')" % (newAlbum.title, newAlbum.artist, newAlbum.genre, newAlbum.releaseDate, newAlbum.rating, newAlbum.tags)
+    
+    try:
+      cursor.execute(sql)
+      db.commit()
+    except MySQLdb.Error, e:
+      print(e)
+      
+    db.close()
+  
+  def deleteAlbum(self, albumID):
+    db = MySQLdb.connect("localhost","root","bre9ase4","TESTDB")
+    cursor = db.cursor()
+    sql = "DELETE FROM albums WHERE albumID = '%d'" % (albumID)
+    
+    try:
+      cursor.execute(sql)
+      db.commit()
+    except MySQLdb.Error, e:
+      print(e)
+      
+    db.close()
+  
+  def deleteAllAlbums(self):
+    db = MySQLdb.connect("localhost","root","bre9ase4","TESTDB")
+    cursor = db.cursor()
+    sql = "TRUNCATE TABLE albums"
+    
+    try:
+      cursor.execute(sql)
+      db.commit()
+    except MySQLdb.Error, e:
+      print(e)
+      
+    db.close()
+    
+  def viewAlbums(self):
+    db = MySQLdb.connect("localhost","root","bre9ase4","TESTDB")
+    cursor = db.cursor()
+    sql = "SELECT * FROM albums"
+    
+    try:
+      cursor.execute(sql)
+      results = cursor.fetchall()
+      for row in results:
+        n = album(row[1],
+                  row[2],
+                  row[3],
+                  row[4],
+                  str(row[5]),
+                  row[6])
+        n.toString()
+    except MySQLdb.Error, e:
+      print(e)
+      
+    db.close()
--- musicAnalyzer.py
+++ musicAnalyzer.py
@@ -1,1 +1,33 @@
-from model import album
+import sys
+from model import *
+from ctrla import *
+
+albumCtrla = ctrla()
+
+while True:
+  albumCtrla.viewAlbums()
+  
+  answer = input("""1 - Add Album
+2 - Delete Album
+3 - Delete All
+4 - Exit
+""")
+
+  if answer == 1:
+    title = raw_input("Title? ")
+    artist = raw_input("Artist? ")
+    genre = raw_input("Genre? ")
+    releaseDate = raw_input("Release Date? ")
+    rating = input("Rating? ")
+    tags = raw_input("Tags? ")
+    
+    x = album(title, artist, genre, releaseDate, rating, tags)
+    albumCtrla.addAlbum(x)
+  elif answer == 2:
+    print("remalbum")
+  elif answer == 3:
+    m = raw_input("Are you sure? ")
+    if m == "Y":
+      albumCtrla.deleteAllAlbums()
+  elif answer == 4:
+    sys.exit()
