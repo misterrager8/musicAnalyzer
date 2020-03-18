056f9303e314351c72b0c766afa15b9882f8a9c3
Chemllei Joseph <chemllei0304@gmail.com>
2020-03-18 01:21:05 -0400

    minor tweaks for cl main menu

--- modules/CLview.py
+++ modules/CLview.py
@@ -7,35 +7,33 @@ genres = ["Hip-Hop/Rap",
           "Alternative",
           "Rock",
           "Soundtrack"]
+mainOptions = ["View All",
+               "Add Album",
+               "Delete Album",
+               "Delete All",
+               "Edit Album",
+               "Export",
+               "Import",
+               "Search",
+               "Exit"]
 
 class homePrompts():
   def __init__(self):
     while True:
-      answer = input(
-"""
-0 - View All Albums
-1 - Add Album
-2 - Delete Album
-3 - Delete All
-4 - Edit Album
-5 - Export
-6 - Import
-7 - Search
-8 - Exit
---------------------
-""")
+      self.printAlbums()
+      self.printOptions(mainOptions)
+      answer = input("What do you want to do? ")
 
-      if answer == 0:
+      if mainOptions[answer] == "View All":
         self.printAlbums()
-      elif answer == 1:
+      elif mainOptions[answer] == "Add Album":
         title = raw_input("Title? ")
         artist = raw_input("Artist? ")
 
-        for idx, item in enumerate(genres):
-          print(str(idx) + " - " + item)
-        genre = input("------------\nGenre? ")
-
+        self.printOptions(genres)
+        genre = input("Genre? ")
         releaseDate = raw_input("Release Date? (m/d/yyyy) ")
+        
         try:
           #TODO: rating is rounding up/down on input, needs fix
           rating = input("Rating? (1-5) ")
@@ -46,36 +44,37 @@ class homePrompts():
 
         x = album(None, title, artist, genres[genre], releaseDate, rating, tags)
         albumCtrla.addAlbum(x)
-        self.printAlbums()
-      elif answer == 2:
-        self.printAlbums()
+        
+      elif mainOptions[answer] == "Delete Album":
         whichAlbum = input("Which Album? ")
         albumCtrla.deleteAlbum(whichAlbum)
-        self.printAlbums()
-      elif answer == 3:
+        
+      elif mainOptions[answer] == "Delete All":
         m = raw_input("Are you sure? ")
         if m == "Y" or m == "y":
           albumCtrla.deleteAllAlbums()
-      elif answer == 4:
-        self.printAlbums()
+      elif mainOptions[answer] == "Edit Album":
         whichAlbum = input("Which Album? ")
         searchType = input("0 - Change title | 1 - Change artist | 2 - Change release date | 3 - Change rating | 4 - Change tags\n")
         change = raw_input("Change? ")
         albumCtrla.editAlbum(whichAlbum, searchType, change)
-        self.printAlbums()
-      elif answer == 5:
+      elif mainOptions[answer] == "Export":
         albumCtrla.exportAlbums()
-      elif answer == 6:
+      elif mainOptions[answer] == "Import":
         albumCtrla.importAlbums()
-      elif answer == 7:
+      elif mainOptions[answer] == "Search":
         searchType = input("0 - By title | 1 - By artist | 2 - By tags | 3 - By year\n")
         searchTerm = raw_input("Search query: ")
         r = albumCtrla.searchAlbums(searchTerm, searchType)
         print(str(len(r)) + " result(s) found.")
         for i in r:
           i.toString()
-      elif answer == 8:
+      elif mainOptions[answer] == "Exit":
         sys.exit()
+        
+  def printOptions(self, ls):
+    for idx, i in enumerate(ls):
+      print(str(idx) + " - " + i)
 
   def printAlbums(self):
     r = albumCtrla.viewAlbums()
