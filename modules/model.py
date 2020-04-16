92a2a12fdac00e5bf2742142fdaba7232ce98f66
Chemllei Joseph <chemllei0304@gmail.com>
2020-04-16 16:17:31 -0400

    song, artist classes added

--- modules/model.py
+++ modules/model.py
@@ -21,6 +21,7 @@ class Album:
         self.rating = rating
         self.tags = tags
         self.genius_url = genius_url
+        self.tracklist = []
 
     def get_title(self):
         return self.title
@@ -43,6 +44,9 @@ class Album:
     def get_genius_url(self):
         return self.genius_url
 
+    def get_tracklist(self):
+        return self.tracklist
+
     def set_title(self, title):
         self.title = title
 
@@ -64,6 +68,9 @@ class Album:
     def set_genius_url(self, genius_url):
         self.genius_url = genius_url
 
+    def set_tracklist(self, tracklist):
+        self.tracklist = tracklist
+
     def to_string(self):
         print(self.album_id,
               str(self.title),
@@ -73,3 +80,72 @@ class Album:
               str(self.rating),
               str(self.tags),
               str(self.genius_url))
+
+
+class Song:
+    def __init__(self, name, album, rating):
+        self.name = name
+        self.album = album
+        self.rating = rating
+
+    def get_name(self):
+        return self.name
+
+    def get_album(self):
+        return self.album.title
+
+    def get_artist(self):
+        return self.album.artist
+
+    def get_rating(self):
+        return self.rating
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_album(self, album):
+        self.album = album
+
+    def set_rating(self, rating):
+        self.rating = rating
+
+    def to_string(self):
+        print(self.name,
+              self.rating)
+
+
+class Artist:
+    def __init__(self, stage_name):
+        self.stage_name = stage_name
+        self.full_name = None
+        self.dob = None
+        self.discog = []
+
+    def get_stage_name(self):
+        return self.stage_name
+
+    def get_full_name(self):
+        return self.full_name
+
+    def get_dob(self):
+        return self.dob
+
+    def get_discog(self):
+        return self.discog
+
+    def set_stage_name(self, stage_name):
+        self.stage_name = stage_name
+
+    def set_full_name(self, full_name):
+        self.full_name = full_name
+
+    def set_dob(self, dob):
+        self.dob = dob
+
+    def set_discog(self, discog):
+        self.discog = discog
+
+    def to_string(self):
+        print(self.stage_name,
+              self.full_name,
+              self.dob)
