9b15f4f9785246d3da6d257b804b6ec455647ff7
Chemllei Joseph <chemllei0304@gmail.com>
2021-02-20 23:15:32 -0500

    Implement SQLAlchemy for better data handling in MySQL
     - Started work bringing in SQLAlchemy for better coupling and modularity

--- modules/ctrla.py
+++ modules/ctrla.py
@@ -2,47 +2,31 @@ import os
 
 import MySQLdb
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
 
 class DB:
     def __init__(self):
         load_dotenv()
+        db_host = os.getenv("host")
+        db_user = os.getenv("user")
+        db_passwd = os.getenv("passwd")
+        db_name = os.getenv("db")
 
-    @staticmethod
-    def db_read(stmt: str) -> list:
-        db = MySQLdb.connect(os.getenv("host"),
-                             os.getenv("user"),
-                             os.getenv("passwd"),
-                             os.getenv("db"))
-        cursor = db.cursor()
-        cursor.execute(stmt)
-        return cursor.fetchall()
+        self.db = create_engine(f'mysql://{db_user}:{db_passwd}@{db_host}/{db_name}')
 
-    @staticmethod
-    def db_write(stmt: str):
-        db = MySQLdb.connect(os.getenv("host"),
-                             os.getenv("user"),
-                             os.getenv("passwd"),
-                             os.getenv("db"))
-        cursor = db.cursor()
-        cursor.execute(stmt)
-        db.commit()
+    def read(self, stmt: str) -> list:
+        try:
+            cursor = self.db.cursor()
+            cursor.execute(stmt)
+            return cursor.fetchall()
+        except MySQLdb.Error as e:
+            print(e)
 
-    def create(self, new_object):
-        stmt = "INSERT INTO - () VALUES ()" % new_object
-        self.db_write(stmt)
-        print("Added.")
-
-    def read(self):
-        stmt = "SELECT * FROM -"
-        self.db_read(stmt)
-
-    def update(self, object_id):
-        stmt = "UPDATE - SET - = - WHERE _id = '%d'" % object_id
-        self.db_write(stmt)
-        print("Updated.")
-
-    def delete(self, object_id):
-        stmt = "DELETE FROM - WHERE _id = '%d'" % object_id
-        self.db_write(stmt)
-        print("Deleted.")
-
+    def write(self, stmt: str):
+        try:
+            cursor = self.db.cursor()
+            cursor.execute(stmt)
+            self.db.commit()
+        except MySQLdb.Error as e:
+            print(e)
--- modules/model.py
+++ modules/model.py
@@ -1,11 +1,26 @@
-class Album:
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Album(Base):
+    __tablename__ = "albums"
+
+    title = Column(String)
+    artist_id = Column(Integer)  # FK
+    genre = Column(String)
+    release_date = Column(String)
+    rating = Column(Integer)
+    album_id = Column(Integer, primary_key=True)
+
     def __init__(self,
                  title: str,
-                 artist_id: int, # FK
+                 artist_id: int,
                  genre: str,
                  release_date: str = None,
                  rating: int = None,
-                 album_id: int = None):#PK
+                 album_id: int = None):
         self.title = title
         self.artist_id = artist_id
         self.genre = genre
@@ -14,27 +29,44 @@ class Album:
         self.album_id = album_id
 
 
-class Artist:
+class Artist(Base):
+    __tablename__ = "artists"
+
+    name = Column(String)
+    hometown = Column(String)
+    dob = Column(String)
+    artist_id = Column(Integer, primary_key=True)
+
     def __init__(self,
                  name: str,
                  hometown: str = None,
                  dob: str = None,
-                 artist_id: int = None): # PK
+                 artist_id: int = None):
         self.name = name
         self.hometown = hometown
         self.dob = dob
         self.artist_id = artist_id
 
 
-class Song:
+class Song(Base):
+    __tablename__ = "songs"
+
+    name = Column(String)
+    artist_id = Column(Integer)  # FK
+    album_id = Column(Integer)  # FK
+    play_count = Column(Integer)
+    rating = Column(Integer)
+    last_played = Column(String)
+    song_id = Column(Integer, primary_key=True)
+
     def __init__(self,
                  name: str,
-                 artist_id: int = None, # FK
-                 album_id: int = None, # FK
+                 artist_id: int = None,
+                 album_id: int = None,
                  play_count: int = None,
                  rating: int = None,
                  last_played: str = None,
-                 song_id: int = None): # PK
+                 song_id: int = None):
         self.name = name
         self.artist_id = artist_id
         self.album_id = album_id
