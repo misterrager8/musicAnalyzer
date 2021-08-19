0ec53a7e61f83bbb6a53641a1b78294c8216271b
Chemllei Joseph <chemllei0304@gmail.com>
2021-08-18 23:26:20 -0400

    get_time_posted fn for datetime formatting

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 import dotenv
 from flask import Flask
@@ -18,3 +19,10 @@ app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql://{db_user}:{db_passwd}@{db_host
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+
+def get_time_posted(date_):
+    return datetime.utcfromtimestamp(float(date_)).strftime("%B %-d, %Y %I:%M %p")
+
+
+app.jinja_env.globals.update(get_time_posted=get_time_posted)
